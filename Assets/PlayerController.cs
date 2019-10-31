65f2b624e842ef57e60c873ab5f9a55905c84889
l9811598117iu <55218520+l9811598117iu@users.noreply.github.com>
2019-10-31 09:22:39 +0800

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    Animator Anim;
+	// Use this for initialization
+	void Start () {
+        Anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.R))
+        {
+            Anim.SetInteger("Anim", 1);
+        }
+        else
+        {
+            Anim.SetInteger("Anim", 0);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Anim.SetInteger("Anim", 2);
+        }
+    }
+}
